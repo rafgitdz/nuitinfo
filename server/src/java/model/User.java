f2d51e42eabb0e52666ba127a9021702aec42b6c
Michelle Avomo Ebolo <mavomoeb@hal.emi.u-bordeaux1.fr>
2011-12-01 21:04:53 +0100

    Adding netbeans project, model par

--- server/src/java/model/User.java
+++ server/src/java/model/User.java
@@ -23,6 +23,12 @@ public class User {
     private String _password;
     private Boolean _sex;
     private List<Integer> _friends;
+    private List<Wish> _wishList;
+
+    public User() {
+        _friends = new ArrayList<Integer>();
+        _wishList = new ArrayList<Wish>();
+    }
 
     public User(String name, String lastname, String email, String nickname,
                 String password) {
@@ -32,6 +38,48 @@ public class User {
         _nickname = nickname;
         _password = password;
         _friends = new ArrayList<Integer>();
+        _wishList = new ArrayList<Wish>();
+    }
+
+    public User(String name, String lastname, Date birthDate, String email,
+                String nickname, String password, Boolean sex) {
+        _name = name;
+        _lastname = lastname;
+        _birthDate = birthDate;
+        _email = email;
+        _nickname = nickname;
+        _password = password;
+        _sex = sex;
+        _friends = new ArrayList<Integer>();
+        _wishList = new ArrayList<Wish>();
+    }
+/******************************  GETTERS **************************************/
+    public Date getBirthDate() {
+        return _birthDate;
+    }
+
+    public String getEmail() {
+        return _email;
+    }
+
+    public List<Integer> getFriends() {
+        return _friends;
+    }
+
+    public String getLastname() {
+        return _lastname;
+    }
+
+    public String getNickname() {
+        return _nickname;
+    }
+
+    public String getPassword() {
+        return _password;
+    }
+
+    public Boolean getSex() {
+        return _sex;
     }
 
     
@@ -39,25 +87,63 @@ public class User {
         return _id;
     }
 
-    public void setId(Integer id) {
-        _id = id;
+/******************************  SETTERS **************************************/
+
+    public void setBirthDate(Date _birthDate) {
+        this._birthDate = _birthDate;
     }
 
-    public String getName(){
-        return null;
-        
+    public void setEmail(String _email) {
+        this._email = _email;
     }
 
+    public void setFriends(List<Integer> _friends) {
+        this._friends = _friends;
+    }
 
-    
+    public void setId(Integer _id) {
+        this._id = _id;
+    }
+
+    public void setLastname(String _lastname) {
+        this._lastname = _lastname;
+    }
+
+    public void setName(String _name) {
+        this._name = _name;
+    }
+
+    public void setNickname(String _nickname) {
+        this._nickname = _nickname;
+    }
+
+    public void setPassword(String _password) {
+        this._password = _password;
+    }
+
+    public void setSex(Boolean _sex) {
+        this._sex = _sex;
+    }
+
+/*****************************************************************************/
+
     public void addFriend(Integer id) {
         _friends.add(id);
     }
 
+    public void removeFriend(Integer id) {
+        _friends.remove(id);
+    }
 
+    public void addWish(Wish wish) {
+        _wishList.add(wish);
+    }
 
+    public void removeWish(Wish wish) {
+        _wishList.remove(wish);
+    }
 
-
-
-
+    public void removeWish(Integer position) {
+        _wishList.remove(position);
+    }
 }
--- server/src/java/model/Wish.java
+++ server/src/java/model/Wish.java
@@ -4,10 +4,94 @@
 
 package model;
 
+import java.util.List;
+import java.util.ArrayList;
+
 /**
  *
  * @author mavomoeb
  */
 public class Wish {
+    private Integer _id;
+    private String _name;
+    private String _description;
+    private List<Category> _categories;
+
+    public Wish() {
+        this._categories = new ArrayList<Category>();
+    }
+    
+    public Wish(Integer id, String name, String description) {
+        this._id = id;
+        this._name = name;
+        this._description = description;
+        this._categories = new ArrayList<Category>();
+    }
+
+    public Wish(Integer id, String name, String description,
+                List<Category> categories) {
+        this._id = id;
+        this._name = name;
+        this._description = description;
+        this._categories = categories;
+    }
+
+/******************************  GETTERS **************************************/
+
+    public List<Category> getCategories() {
+        return _categories;
+    }
+
+    public Category getCategory(int position) {
+        return _categories.get(position);
+    }
+
+    public String getDescription() {
+        return _description;
+    }
+
+    public Integer getId() {
+        return _id;
+    }
+
+    public String getName() {
+        return _name;
+    }
+
+/******************************  SETTERS **************************************/
+
+    public void setCategories(List<Category> categories) {
+        this._categories = categories;
+    }
+
+    public void setCategory(int position, Category category) {
+        this._categories.set(position, category);
+    }
+
+    public void setDescription(String description) {
+        this._description = description;
+    }
+
+    public void setId(Integer id) {
+        this._id = id;
+    }
+
+    public void setName(String name) {
+        this._name = name;
+    }
+
+/*****************************************************************************/
+
+    public void addCategory(Category category) {
+        this._categories.add(category);
+    }
+
+    public void removeCategory(Category category) {
+        this._categories.remove(category);
+    }
+
+    public void removeCategory(Integer position) {
+        this._categories.remove(position);
+    }
 
 }
