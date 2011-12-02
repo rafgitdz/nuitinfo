1c3e14fb0fc4814eaa7fa98f18ad79db9671bfb9
Matthieu Foucault <foucault.matthieu@gmail.com>
2011-12-02 02:36:54 +0100

    add userFactory

---
+++ server/Gifts4YourFriends/ejbModule/model/user/UserFactory.java
@@ -0,0 +1,22 @@
+package model.user;
+
+import java.util.Date;
+import java.util.List;
+
+import model.Wish;
+
+public class UserFactory {
+
+	public User newUser(String name, String lastname, Date birthday, String mail,String nickname,String pwd,Integer sex, List<User> friends,
+		    List<Wish> wishlist) {
+		
+		User usr = new User(name, lastname, mail, nickname, pwd, sex);
+		for (User friend : friends)
+			usr.addFriend(friend);
+		usr.setWishList(wishlist);
+		
+		return null;
+		
+	}
+	
+}
